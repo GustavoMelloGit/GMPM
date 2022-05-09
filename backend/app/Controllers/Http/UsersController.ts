2da9e7dae937a532de9d4bcf5a51684a05ef6bf9
Gustavo Marques <gugamello2014@yahoo.com>
2022-05-09 09:04:28 -0300

    feat: :sparkles: backend added

---
+++ backend/app/Controllers/Http/UsersController.ts
@@ -0,0 +1,19 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
+export default class UsersController {
+  public async create({ request }: HttpContextContract) {
+    const { name, email, password } = request.only(['name', 'email', 'password'])
+    const user = await User.create({
+      name,
+      email,
+      password,
+    })
+    return user
+  }
+
+  public async index() {
+    const all = await User.all()
+    return all.map((user) => ({ name: user.name, email: user.email }))
+  }
+}
