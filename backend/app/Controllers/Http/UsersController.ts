b5eb02f30c1362081d6b7cf1f3774d4d064d5d44
Gustavo Marques <gugamello2014@yahoo.com>
2022-05-14 16:55:43 -0300

    feat: :sparkles: authentication and findOne route

--- backend/app/Controllers/Http/UsersController.ts
+++ backend/app/Controllers/Http/UsersController.ts
@@ -4,16 +4,33 @@ import User from 'App/Models/User'
 export default class UsersController {
   public async create({ request }: HttpContextContract) {
     const { name, email, password } = request.only(['name', 'email', 'password'])
-    const user = await User.create({
-      name,
-      email,
-      password,
-    })
-    return user
+    try {
+      const user = await User.create({
+        name,
+        email,
+        password,
+      })
+      return user
+    } catch (e) {
+      return { error: e }
+    }
   }
 
-  public async index() {
-    const all = await User.all()
-    return all.map((user) => ({ name: user.name, email: user.email, uuid: user.uuid }))
+  public async read() {
+    try {
+      const all = await User.all()
+      return all.map((user) => ({ name: user.name, email: user.email }))
+    } catch (e) {
+      return { error: e }
+    }
+  }
+
+  public async findOne({ params }: HttpContextContract) {
+    try {
+      const user = await User.findOrFail(params.uuid)
+      return { name: user.name, email: user.email, uuid: user.uuid }
+    } catch (e) {
+      return { error: e }
+    }
   }
 }
--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -25,16 +25,7 @@ Route.get('/', async () => {
 })
 
 Route.post('/users', 'UsersController.create')
-Route.get('/users', 'UsersController.index')
+Route.get('/users', 'UsersController.read')
+Route.get('/users/:uuid', 'UsersController.findOne')
 
-Route.post('/login', async ({ auth, request, response }) => {
-  const email = request.input('email')
-  const password = request.input('password')
-
-  try {
-    const token = await auth.use('api').attempt(email, password)
-    return token
-  } catch {
-    return response.badRequest('Invalid credentials')
-  }
-})
+Route.post('/login', 'AuthController.login')
