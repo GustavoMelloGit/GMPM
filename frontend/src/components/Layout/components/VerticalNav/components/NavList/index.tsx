9f79142fd2436738133919e774e9b038b3f8446e
Gustavo Marques <gugamello2014@yahoo.com>
2022-05-31 09:18:20 -0300

    feat: :sparkles: mnu items now close the drawer when clicked

--- frontend/src/components/Layout/components/VerticalNav/components/NavList/index.tsx
+++ frontend/src/components/Layout/components/VerticalNav/components/NavList/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ListItem, Box } from '@mui/material';
 import { navItems } from '../../../../../../routes/routesConfig';
 import { useLocation, useNavigate } from 'react-router-dom';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import {
   NavListIcon,
   NavListItemButton,
   NavListList,
   NavListText,
 } from './styles';
+import { LayoutContext } from '../../../../../../contexts/layout';
 
-const NavList: React.FC = (props) => {
+const NavList: React.FC = () => {
+  const { toggleDrawer } = useContext(LayoutContext);
   const navigate = useNavigate();
   const location = useLocation();
+
+  const handleItemClick = (path: string) => {
+    navigate(path);
+    toggleDrawer();
+  };
 
   return (
     <Box component='nav'>
@@ -22,11 +30,12 @@ const NavList: React.FC = (props) => {
           return (
             <ListItem disablePadding key={item.id}>
               <NavListItemButton
-                onClick={() => navigate(item.url)}
+                onClick={handleItemClick.bind(this, item.url)}
                 selected={active}
               >
                 {item.icon && <NavListIcon>{item.icon}</NavListIcon>}
                 <NavListText primary={item.title} />
+                <ArrowForwardIosIcon sx={{ height: '15px' }} />
               </NavListItemButton>
             </ListItem>
           );
